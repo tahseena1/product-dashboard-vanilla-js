0fae435c41fbc5a7b940ab922460f1335f68a330
Tahseena Badat <tahseena@usf.edu>
2025-07-20 23:49:27 +0200

    created fetchProductsAsync() function

--- cp_2.js
+++ cp_2.js
@@ -17,3 +17,17 @@ function fetchProductsThen() {
 }
 
 fetchProductsThen();
+
+async function fetchProductsAsync() {
+    try{
+        const response = await fetch('https://www.course-api.com/javascript-store-products');
+        if (!response.ok) {
+            throw new Error('Oh no, something went wrong!');
+        }
+        const data = await response.json();
+        displayProducts(data);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
